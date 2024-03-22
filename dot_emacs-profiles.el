7109a2e38550681de7226669e788b8aa060a7d34
Mike Barker <mike@thebarkers.com>
2024-03-22 15:58:36 -0600

    emacs: Add emacs configurations git repos and chemacs2 config

---
+++ dot_emacs-profiles.el
@@ -0,0 +1,4 @@
+(("efs" . ((user-emacs-directory . "~/.config/emacs-efs/home/.emacs.d")))
+ ("min" . ((user-emacs-directory . "~/.config/emacs-minimal/home/.emacs.d")))
+ ("old" . ((user-emacs-directory . "~/.config/emacs/home/.emacs.d")))
+ ("org" . ((user-emacs-directory . "~/.config/emacs-org/home/.emacs.d"))))
