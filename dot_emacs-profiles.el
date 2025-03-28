9b45792584bf9bcf187210652714d88dd4b2778f
Mike Barker <mike@thebarkers.com>
2025-03-28 15:39:12 -0600

    emacs: update emacs config
    - removed chemacs2
    - moved my personal emacs github repos from .config
      to ~/github/mrxcitement

--- dot_emacs-profiles.el
+++
@@ -1,4 +0,0 @@
-(("efs" . ((user-emacs-directory . "~/.config/emacs-efs/home/.emacs.d")))
- ("min" . ((user-emacs-directory . "~/.config/emacs-minimal/home/.emacs.d")))
- ("old" . ((user-emacs-directory . "~/.config/emacs/home/.emacs.d")))
- ("org" . ((user-emacs-directory . "~/.config/emacs-org/home/.emacs.d"))))
