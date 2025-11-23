b0b931cf2c9fcadd87c9351177b5cc4557e330be
Mike Barker <mike@thebarkers.com>
2025-11-22 21:29:24 -0700

    emacs: fix error with use-package vc:
    On Emacs versions < 30 use-package does not have the ability to load a
    package from version control, vc: and needs to be not loaded. Since
    use-package is a macro it needs to be quoted to only have it evaluated
    when the Emacs version is >= 30. Quoting prevents macro expansion until
    runtime, when the guard has already passed.

--- dot_config/emacs/lisp/packages.el
+++ dot_config/emacs/lisp/packages.el
@@ -226,14 +226,16 @@
 ;;; flyspell-markdownlint-cli2
 ;; Lint your markdown in Emacs with flymake and markdownlint-cli2
 ;; https://github.com/ewilderj/flymake-markdownlint-cli2
-(use-package flymake-markdownlint-cli2
-  :vc (:url "https://github.com/ewilderj/flymake-markdownlint-cli2"
-            :rev :newest
-            :branch "main")
-  :config
-  (add-hook 'markdown-mode-hook 'flymake-mode)
-  (add-hook 'markdown-mode-hook 'flymake-markdownlint-cli2-setup))
-
+(when (> emacs-major-version 29)
+  (eval
+   '(use-package flymake-markdownlint-cli2
+      :vc (:url "https://github.com/ewilderj/flymake-markdownlint-cli2"
+		:rev :newest
+		:branch "main")
+      :config
+      (add-hook 'markdown-mode-hook 'flymake-mode)
+      (add-hook 'markdown-mode-hook 'flymake-markdownlint-cli2-setup))
+   ))
 ;;; flyspell-popup
 ;; A mode to show flyspell correction choices in a popup
 (use-package flyspell-popup
