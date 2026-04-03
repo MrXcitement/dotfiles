d4e4c25a202c6960ca29a810c6777e155f303b3b
Mike Barker <mike@thebarkers.com>
2026-04-03 15:05:26 -0600

    powershell: reformat profile.d/mise.ps1

--- dot_config/powershell/profile.d/mise.ps1
+++ dot_config/powershell/profile.d/mise.ps1
@@ -4,10 +4,10 @@
 # July 15th, 2025
 
 if (Get-Command mise -ErrorAction SilentlyContinue) {
-    mise activate pwsh | Out-String | Invoke-Expression
-    # Add mise shims to PATH so all the downloaded packages work correctly
-    $shim = "$env:LOCALAPPDATA\mise\shims"
-    if (Test-Path $shim) {
-	$env:PATH = "$shim;$env:PATH"
-    }
+  mise activate pwsh | Out-String | Invoke-Expression
+  # Add mise shims to PATH so all the downloaded packages work correctly
+  $shim = "$env:LOCALAPPDATA\mise\shims"
+  if (Test-Path $shim) {
+    $env:PATH = "$shim;$env:PATH"
+  }
 }
