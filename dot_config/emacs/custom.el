afc81fe2dfb3e961a97f3c8f41b9c98985df0477
Mike Barker <mike@thebarkers.com>
2026-08-10 11:26:31 -0600

    emacs: add customize file

---
+++ dot_config/emacs/custom.el
@@ -0,0 +1,27 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("2f387f2d0418cda2d9a66f3930f4d5886396660cc4547450387b9ea9ee72e85a"
+     "9f04585d6f52b358eac2efc88eebdf9cb525af51f130fc1dcf4f53753f77cb83"
+     "e1da45d87a83acb558e69b90015f0821679716be79ecb76d635aafdca8f6ebd4"
+     "f2bfb91e0aa3933260b0a436e2a46d5ef8b2fa2ecd6935d2d4a5f00e605dcf55"
+     "e7b0f33dfdebf6e8950d75aa6400bb7f84832838ebc95a43c719f1ee86f75294"
+     default))
+ '(my-theme-dark 'modus-vivendi)
+ '(my-theme-light 'modus-operandi)
+ '(package-selected-packages
+   '(cape consult corfu eat embark evil-collection evil-surround
+	  exec-path-from-shell marginalia markdown-mode orderless
+	  undo-tree vertico yasnippet-snippets))
+ '(tramp-term-host-shells '(("devbox" . bash)))
+ '(warning-suppress-types '((use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
