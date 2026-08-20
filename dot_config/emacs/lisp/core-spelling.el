250cec59ca2fff12dd556007e7a88bdc18c94b86
Mike Barker <mike@thebarkers.com>
2026-08-20 15:19:39 -0600

    emacs: will use aspell if installed, then hunspell

--- dot_config/emacs/lisp/core-spelling.el
+++ dot_config/emacs/lisp/core-spelling.el
@@ -2,10 +2,10 @@
 
 ;; Mike Barker <mike@thebarkers.com>
 ;; Created: November 24th, 2025
-;; Updated: August 8th, 2026
+;; Updated: August 20th, 2026
 
 ;;; Commentary:
-;; When the spelling program exists, initialize the spelling system.
+;; If `aspell' is installed, use it, otherwise when `hunspell' is installed configure it.
 
 ;;; History:
 ;; See my dotfiles repo and the emacs folder
@@ -13,20 +13,19 @@
 
 ;;; Code:
 ;;; Configure spelling
-(use-package ispell
-  :straight nil
-  :if (executable-find "hunspell")
-
-  :init
-  (setq ispell-program-name "hunspell")
-  (setq ispell-local-dictionary "en_US")
-
-  :config
-  (when (eq system-type 'darwin))
-  (when (eq system-type 'gnu/linux))
-  (when (eq system-type 'windows-nt)
-    (setenv "LANG" "en_US")
-    (setopt ispell-hunspell-dict-paths-alist '(("en_US" "c:/hunspell/en_US.aff")))))
+(if (executable-find "aspell")
+    (progn
+      ;;(setq ispell-dictionary "your_default_dictionary")
+      (setq ispell-program-name "aspell")
+      (setq ispell-silently-savep t))
+  (when (executable-find "hunspell")
+    (setq ispell-program-name "hunspell")
+    (setq ispell-local-dictionary "en_US")
+    (when (eq system-type 'darwin))
+    (when (eq system-type 'gnu/linux))
+    (when (eq system-type 'windows-nt)
+      (setenv "LANG" "en_US")
+      (setopt ispell-hunspell-dict-paths-alist '(("en_US" "c:/hunspell/en_US.aff"))))))
 
 (provide 'core-spelling)
 ;; core-spelling.el ends here.
