dc73337f5650f19b9607f18fc5271913b234dd4e
Mike Barker <mike@thebarkers.com>
2026-08-31 16:30:16 -0600

    emacs: optimize dired configuration

--- dot_config/emacs/lisp/core-dired.el
+++ dot_config/emacs/lisp/core-dired.el
@@ -2,11 +2,13 @@
 
 ;; Mike Barker <mike@thebarkers.com>
 ;; Created: November 24th, 2025
-;; Updated: August 13th, 2026
+;; Updated: August 30th, 2026
 
 ;;; Commentary:
 ;; Any user customizations to the `dired' mode should go here.
 ;; dired.el, dired-aux.el, dired-x.el, ls-lisp.el
+;; James Cherti has some useful configuration recomendations here:
+;; https://www.jamescherti.com/emacs-dired-configuration/
 
 ;;; History:
 ;; See my dotfiles repo and the emacs folder
@@ -14,16 +16,48 @@
 
 ;;; Code:
 
-(setq dired-free-space nil
-      dired-dwim-target t  ; Propose a target for intelligent moving/copying
-      dired-deletion-confirmer 'y-or-n-p
-      dired-filter-verbose nil
-      dired-recursive-deletes 'top
-      dired-recursive-copies 'always
-      dired-vc-rename-file t
+;; Configure dired behavior
+(setq dired-clean-confirm-killing-deleted-buffers nil
       dired-create-destination-dirs 'ask
-      ;; Suppress Dired buffer kill prompt for deleted dirs
-      dired-clean-confirm-killing-deleted-buffers nil)
+      dired-deletion-confirmer 'y-or-n-p
+      dired-dwim-target t  ; Propose a target for intelligent moving/copying
+      dired-filter-verbose nil
+      dired-free-space nil
+      dired-kill-when-opening-new-dired-buffer t
+      dired-mouse-drag-files t
+      dired-movement-style 'bounded-files
+      dired-recursive-copies 'always
+      dired-recursive-deletes 'top
+      dired-vc-rename-file t)
+
+;; Keep dired clean by hiding dotfiles
+(setq dired-omit-verbose nil)
+(setq dired-omit-files (concat "\\`[.]\\'"
+                               "\\|^\\."))
+(add-hook 'dired-mode-hook #'dired-omit-mode)
+
+;; Hide details
+(add-hook 'dired-mode-hook #'dired-hide-details-mode)
+
+;; Highlight the current line when in dired mode.
+(add-hook 'dired-mode-hook 'hl-line-mode)
+
+;; Sort directories first 
+(setq ls-lisp-verbosity nil)
+(setq ls-lisp-dirs-first t)
+
+;; The `ls' command on darwin and bsd systems doesn't support --dired
+(when (or (eq system-type 'darwin) (eq system-type 'berkeley-unix))
+  (setq dired-use-ls-dired nil))
+
+;; On `darwin' or `bsd' systems, if `gls' is installed use it with the `gls-args'.
+(let ((gls-args "--group-directories-first -ahlv"))
+  (when (or (eq system-type 'darwin) (eq system-type 'berkeley-unix))
+    (if-let* ((gls (executable-find "gls")))
+        (setq insert-directory-program gls)
+      (setq gls-args nil)))
+  (when gls-args
+    (setq dired-listing-switches gls-args)))
 
 ;; This is a higher-level predicate that wraps `dired-directory-changed-p'
 ;; with additional logic. This `dired-buffer-stale-p' predicate handles remote
@@ -38,23 +72,10 @@
 ;; subdirectory, causing UI freezes on remote/network drives).
 (setq dired-auto-revert-buffer 'dired-directory-changed-p)
 
-;; Automatically revert destination Dired buffers after file operations
-;; (e.g., copying or renaming), but skip remote directories to prevent
-;; TRAMP network latency and UI freezes.
-(defun my--local-dir-p (dir)
-  "Return non-nil if DIR is a local directory."
-  (not (file-remote-p dir)))
-(setq dired-do-revert-buffer #'my--local-dir-p)
-
-;; dired-omit-mode
-(setq dired-omit-verbose nil
-      dired-omit-files (concat "\\`[.]\\'"))
-
-;; Highlight the current line when in dired mode.
-(add-hook 'dired-mode-hook 'hl-line-mode)
-
-(setq ls-lisp-verbosity nil)
-(setq ls-lisp-dirs-first t)
+;; Revert destination Dired buffers after file operations.
+;; Skip remote directories to prevent TRAMP network latency and UI freezes.
+(setq dired-do-revert-buffer (lambda (dir)
+                               (not (file-remote-p dir))))
 
 (provide 'core-dired)
 ;;; End of core-dired.el
