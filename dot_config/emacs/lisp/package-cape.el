3ebd36d212ada6fc1594e50c05b12a88b9f0473c
Mike Barker <mike@thebarkers.com>
2026-09-15 07:01:12 -0600

    emacs: split out package cape from curfu config.

---
+++ dot_config/emacs/lisp/package-cape.el
@@ -0,0 +1,41 @@
+;;; package-cape.el --- Module package-cape.
+
+;; Mike Barker <mike@mbp15-12>
+;; Created: September 15th, 2026
+;; Updated:
+
+;;; Commentary:
+;; Configure package-cape.
+
+;;; History:
+;; See my dotfiles repo and the emacs folder
+;; https://github.com/MrXcitement/dotfiles/tree/main/dot_config/emacs
+
+;;; Code:
+
+;; Completion At Point Extensions
+;; https://github.com/minad/cape
+(use-package cape
+  :ensure t
+  ;; Bind prefix keymap providing all Cape commands under a mnemonic key.
+  ;; Press C-c p ? to for help.
+  :bind ("C-c p" . cape-prefix-map) ;; Alternative key: M-<tab>, M-p, M-+
+  ;; Alternatively bind Cape commands individually.
+  ;; :bind (("C-c p d" . cape-dabbrev)
+  ;;        ("C-c p h" . cape-history)
+  ;;        ("C-c p f" . cape-file)
+  ;;        ...)
+  :init
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
+  ;; (add-hook 'completion-at-point-functions #'cape-dabbrev)
+  ;; (add-hook 'completion-at-point-functions #'cape-file)
+  ;; (add-hook 'completion-at-point-functions #'cape-elisp-block)
+  ;; (add-hook 'completion-at-point-functions #'cape-history)
+  ;; ...
+)
+
+(provide 'package-cape)
+;;; package-cape.el ends here.
