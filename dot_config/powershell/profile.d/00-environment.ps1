8b82a9287e631bc80a059f19b1a986e0d3798317
Mike Barker <mike@thebarkers.com>
2026-09-16 15:37:25 -0600

    powershell: added python utf-8 environment vars

--- dot_config/powershell/profile.d/00-environment.ps1
+++ dot_config/powershell/profile.d/00-environment.ps1
@@ -8,7 +8,16 @@ if (($null -ne $IsWIndows) -and $IsWIndows) {
   # If the user does not have a environment variable HOME defined,
   # Create it using the HOMEDRIVE and HOMEPATH variables.
   if (-Not [Environment]::GetEnvironmentVariable("HOME", "User")) {
-    [Environment]::SetEnvironmentVariable("HOME","$Env:HOMEDRIVE$Env:HOMEPATH", "User")
     $Env:HOME="$Env:HOMEDRIVE$Env:HOMEPATH"
+    [Environment]::SetEnvironmentVariable("HOME", $Env:HOME, "User")
+  }
+  # Python 3.7+ use UTF-8 encoding by default
+  if (-Not [Environment]::GetEnvironmentVariable("PYTHONUTF8", "User")) {
+    $Env:PYTHONUTF8=1
+    [Environment]::SetEnvironmentVariable("PYTHONUTF8", $Env:PYTHONUTF8, "User")
+  }
+  if  (-Not [Environment]::GetEnvironmentVariable("PYTHONIOENCODING", "User")) {
+    $Env:PYTHONIOENCODING="utf-8"
+    [Environment]::SetEnvironmentVariable("PYTHONIOENCODING", $Env:PYTHONIOENCODING, "User")
   }
 }
